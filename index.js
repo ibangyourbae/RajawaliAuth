8e9c7dab59c20da60e8838086df27ed61c9013ee
Muhammad Iqbal <iqbalshedow25@gmail.com>
2021-09-18 22:01:56 +0700

    'update'

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ const authRoute = require('./routes/auth')
 const postRoute = require('./routes/posts')
 const { post } = require('./routes/auth')
 
+// CORS
+var whitelist = ['http://https://platform-pejabat.vercel.app/#/login.com', 'https://platform-pejabat.vercel.app/#/register']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 
 // Connect DB
 mongoose.connect(process.env.DB_CONNECT, 
@@ -21,12 +33,12 @@ mongoose.connect(process.env.DB_CONNECT,
 
 // Middleware
 app.use(express.json())
-app.use(cors())
-app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Methods','GET','POST','PUT','PATCH','DELETE');
-  next(); 
-})
+// app.use(cors())
+// app.use((req,res,next)=>{
+//   res.setHeader('Access-Control-Allow-Origin','*');
+//   res.setHeader('Access-Control-Allow-Methods','GET','POST','PUT','PATCH','DELETE');
+//   next(); 
+// })
 
 // Route Middlewares
 
